7d44b5acf500251bfa7bc13fb8ac9e7f5e41f60c
Alain <alamar1990@gmail.com>
2025-10-07 16:33:40 -0500

    feat: add scss support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,44 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
-    app: {
-      head: {
-        link: [
-          {
-              rel: 'stylesheet',
-              href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
-          }
-        ]
+  
+  // CSS configuration
+  css: [
+    '~/assets/scss/main.scss'
+  ],
+  
+  // Modules
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint'
+  ],
+  
+  // Vite configuration for SCSS
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // This makes SCSS variables available in all components
+          additionalData: `
+            @use "sass:map";
+            @use "sass:list";
+            @use "sass:math";
+            @use "@/assets/scss/_variables.scss" as *;
+          `
+        }
       }
     }
+  },
+  
+  // App configuration
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+        }
+      ]
+    }
+  }
 })
