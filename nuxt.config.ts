afa3a358d7093910a69f1f669828e7e4fff88c9e
Alain <alamar1990@gmail.com>
2025-10-07 15:53:55 -0500

    feat: add tailwind custom conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+    app: {
+      head: {
+        link: [
+          {
+              rel: 'stylesheet',
+              href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+          }
+        ]
+      }
+    }
 })
